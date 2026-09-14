0d7492ee13a00744b5b9af742e18736c917b8e07
IMJNMHTODRLA <sola001.w@gmail.com>
2026-09-14 21:04:08 +0900

    그닥 별 거 없는 거

--- Scripts/InputMap.cs
+++ Scripts/InputMap.cs
@@ -4,10 +4,10 @@ namespace ProjectPQ.Scripts;
 
 public static class InputMap
 {
-    public readonly static StringName W = new("w");
-    public readonly static StringName A = new("a");
-    public readonly static StringName S = new("s");
-    public readonly static StringName D = new("d");
+    public readonly static StringName W = "w";
+    public readonly static StringName A = "a";
+    public readonly static StringName S = "s";
+    public readonly static StringName D = "d";
 
-    public readonly static StringName MouseLeft = new("mouse_left");
+    public readonly static StringName MouseLeft = "mouse_left";
 }
